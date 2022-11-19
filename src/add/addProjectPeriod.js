9cfa754fda9c47db16b3d7d87b1c40f9a9ac4e5b
seonb2n <626ksb@gmail.com>
2022-11-19 09:52:16 +0900

    - react-date-range 달력으로 project 기간 설정하는 화면 구현

--- src/add/addProjectPeriod.js
+++ src/add/addProjectPeriod.js
@@ -15,17 +15,35 @@ function AddProjectPeriod() {
         },
     ]);
 
+    const getDateDiff = (d1, d2) => {
+        const date1 = new Date(d1);
+        const date2 = new Date(d2);
+
+        const diffDate = date1.getTime() - date2.getTime();
+
+        return Math.abs(diffDate / (1000 * 60 * 60 * 24)); // 밀리세컨 * 초 * 분 * 시 = 일
+    }
+
     return (
         <div>
             <DateRange
                 locale={locales["ko"]}
                 editableDateInputs={true}
-                onChange={(item) => setDayRange([item.selection])}
+                onChange={(item) => {
+                    setDayRange([item.selection]);
+                }}
                 moveRangeOnFirstSelection={false}
                 ranges={dayRange}
                 months={1}
                 dateDisplayFormat={'yyyy년 MM월 dd일 E'}
             />
+            <div className="w100p flexCenter">
+                <div className="addPeriodSubmitButton w90p h50 flexCenterAlignHorizon">
+                    <div>
+                        <p>{getDateDiff(dayRange[0].endDate, dayRange[0].startDate) + 1}일 동안 하기</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
