050d7f2d68a5bea71735cb2c9afa72aeb1ef4852
seonb2n <626ksb@gmail.com>
2023-04-23 23:49:30 +0900

    [FEATURE] - 회원가입 중, 하루의 시작 시간과 종료 시간을 설정하는 TimePicker 추가

--- src/login/sighUp/signUpSetDate.js
+++ src/login/sighUp/signUpSetDate.js
@@ -1,4 +1,7 @@
 import {useLocation} from "react-router-dom";
+import "../../css/base.css"
+import {useState} from "react";
+import { TimePicker } from 'react-ios-time-picker';
 
 function SignUpSetDate() {
     const location = useLocation();
@@ -6,10 +9,64 @@ function SignUpSetDate() {
     const userPW = location.state.userPW;
     const userNickName = location.state.userNickName;
 
-    console.log(userNickName);
+    const [startTime, setStartTime] = useState('09:00');
+    const onChangeStartTime = (timeValue) => {
+        setStartTime(timeValue);
+    }
+
+    const [endTime, setEndTime] = useState('18:00');
+    const onChangeEndTime = (timeValue) => {
+        setEndTime(timeValue);
+    }
 
     return (
         <div>
+            <div className="title_area ">
+                <p className="fBold title_txt">Welcome,</p>
+                <div className="flex w100p">
+                    <div className="fBold title_txt">TOODLE</div>
+                    <div className="ml10 title_dot_container">
+                        <div className="bgLightOrange title_dot"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="main_area ml20 mr20 mt15">
+
+                <div className="fs16p fBold">
+                    <div>
+                        {userNickName} 님.
+                    </div>
+                    <div>
+                        <p>
+                            하루의 시작과 종료를 설정할까요?
+                        </p>
+                    </div>
+                </div>
+
+                <div className="mt30 fs20p">
+                    <div className="flex">
+                        <div>하루의</div>
+                        <div className="ml5 fc_lo">시작 시간은</div>
+                    </div>
+                </div>
+
+                <div className="time_picker">
+                    <TimePicker onChange={onChangeStartTime} value={startTime} />
+                </div>
+
+                <div className="mt30 fs20p">
+                    <div className="flex">
+                        <div>하루의</div>
+                        <div className="ml5 fc_lo">종료 시간은</div>
+                    </div>
+                </div>
+
+                <div className="time_picker">
+                    <TimePicker onChange={onChangeEndTime} value={endTime} />
+                </div>
+            </div>
+
 
         </div>
     );
