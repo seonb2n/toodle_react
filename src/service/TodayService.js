79ce5770db53e6b7a6d75ace7ff1cccc2dcbee58
seonb2n <626ksb@gmail.com>
2023-06-03 14:38:41 +0900

    [FEATURE]  - 프로젝트 등록 후에 today 페이지로 이동하도록 구현
    [REFACTORING]
     - TodayService.js 에서 localhost 로 되어 있던 IP 를 동적으로 변경

--- src/service/TodayService.js
+++ src/service/TodayService.js
@@ -1,16 +1,23 @@
 import AuthenticationService from "./AuthenticationService";
 import axios from "axios";
+import IPSetService from "./IPSetService";
 
 class TodayService {
 
+    API_SERVER_URL;
+
+    constructor() {
+        this.API_SERVER_URL = IPSetService.API_IP;
+    }
+
     executeProjectGetService() {
         this.setUpAxiosInterceptors();
-        return axios.get('http://localhost:8080/api/v1/projects');
+        return axios.get(this.API_SERVER_URL + '/api/v1/projects');
     }
 
     executeProjectRegisterService(projectRequest) {
         this.setUpAxiosInterceptors();
-        return axios.post('http://localhost:8080/api/v1/projects/register', {
+        return axios.post(this.API_SERVER_URL + '/api/v1/projects/register', {
             projectId: projectRequest.projectId,
             projectName: projectRequest.projectName,
             taskDtoSet: projectRequest.taskDtoSet
@@ -18,12 +25,18 @@ class TodayService {
             headers: {
                 'Content-Type': 'application/json'
             }
-        });
+        }).then(response => {
+            if (response.status === 200) {
+                // 정상적으로 등록이 된 경우 today 페이지로 redirect 한다.
+                window.location.pathname = '/today'
+            }
+        })
+            ;
     }
 
     executePostItUpdateService(postItDtoList) {
         this.setUpAxiosInterceptors();
-        return axios.post('http://localhost:8080/api/v1/postits/update', {
+        return axios.post(this.API_SERVER_URL + '/api/v1/postits/update', {
             postItDtoList
         });
     }
